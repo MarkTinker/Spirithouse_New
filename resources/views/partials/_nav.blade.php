001a2f7c279e4e9a60a879b919666dc062f905b4
MarkTinker <mwbdevelopment@gmail.com>
2017-07-03 16:01:23 -0600

    update

--- resources/views/partials/_nav.blade.php
+++ resources/views/partials/_nav.blade.php
@@ -1,9 +1,10 @@
-<div id="topheader">
-    <div class="row logobackground">
-        <h1>
-            <a href="{{ route('pages.home') }}" class="logo"> </a>
-        </h1>
-    </div>
+<div class="row logobackground">
+    <h1>
+        <a href="{{ route('pages.home') }}" class="logo"> </a>
+    </h1>
+</div>
+
+<div id="topheader">    
     <nav class="navbar navbar-default">
         <div class="container">
             <div class="navbar-header">
@@ -14,22 +15,26 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 </button>
+                <a class="navbar-brand" href="{{ route('pages.home') }}"><div class="navbar-imgcontainer"><img src="{{ asset('images/logo-text.png') }}"></div></a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
                     <li class="{{ Request::is('restaurant') ? "active" : "" }}"> <a href="{{route('pages.restaurant')}}">Restaurant</a></li>
-                    <li class="{{Request::is('school') ? "active":""}}"> <a href="{{route('pages.school')}}">Cooking School</a></li>
-                    <li class="{{Request::is('shop') ? "active":""}}"> <a href="{{route('pages.shop')}}">Shop</a></li>
-                    <li class="{{Request::is('vouchers') ? "active":""}}"> <a href="{{route('pages.vouchers')}}">Vouchers</a></li>
-                    <li class="{{Request::is('foodtours') ? "active":""}}"> <a href="{{route('pages.foodtours')}}">Food Tours</a></li>
-                    <li class="{{Request::is('mapsandinfo') ? "active":""}}"> <a href="{{route('pages.mapsandinfo')}}">Maps & Info</a></li>
+                    <li class="{{ Request::is('school') ? "active":"" }}"> <a href="{{route('pages.school')}}">Cooking School</a></li>
+                    <li class="{{ Request::is('shop') ? "active":"" }}"> <a href="{{route('pages.shop')}}">Shop</a></li>
+                    <li class="{{ Request::is('vouchers') ? "active":"" }}"> <a href="{{route('pages.vouchers')}}">Vouchers</a></li>
+                    <li class="{{ Request::is('foodtours') ? "active":"" }}"> <a href="{{route('pages.foodtours')}}">Food Tours</a></li>
+                    <li class="{{ Request::is('mapsandinfo') ? "active":"" }}"> <a href="{{route('pages.mapsandinfo')}}">Maps & Info</a></li>
                 </ul>
             </div>
             <!--/.nav-collapse -->
-        </div>
-        <div class="bigtitle container">
-            <h1>@yield ('pagetitle')</h1>
-            <p>@yield ('pagedescription')</p>        
-        </div>
+        </div>        
     </nav>
+</div>
+
+<div id="toptitle">
+    <div class="bigtitle container">
+        <h1>@yield ('pagetitle')</h1>
+        <p>@yield ('pagedescription')</p>        
+    </div>
 </div>
